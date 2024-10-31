15d9baf6cfe22be39be6a77292a98d5c78bf27d3
Jeff Michaud <cometaj2@comcast.net>
2024-10-30 17:47:24 -0700

    update version

--- hcli_hc/package.py
+++ hcli_hc/package.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 dependencies = ["apscheduler==3.10.4",
                 "pyserial==3.5"]
